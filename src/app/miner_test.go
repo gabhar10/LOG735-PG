b79ad52f402a118147c6cd9c38c008158c39f100
Philippe Deschesnes <philippe.deschesnes.1@ens.etsmtl.ca>
2018-07-06 22:55:58 -0400

    Added test for miner

--- src/app/miner_test.go
+++ src/app/miner_test.go
@@ -4,45 +4,51 @@ import (
 	"LOG735-PG/src/node"
 	brpc "LOG735-PG/src/rpc"
 	"testing"
+	"time"
 )
 
 func TestMiner_mining(t *testing.T) {
 	type fields struct {
-		ID              string
-		blocks          []node.Block
-		peers           []string
-		rpcHandler      *brpc.NodeRPC
-		messageQueue    []Message
-		messageChannels []chan Message
-		blocksChannels  []chan node.Block
-		miningBlock     node.Block
-		quit            chan bool
-	}
-	type args struct {
-		block      *node.Block
-		difficulty int
+		ID                string
+		blocks            []node.Block
+		peers             []string
+		rpcHandler        *brpc.NodeRPC
+		incomingMsgChan   chan Message
+		incomingBlockChan chan node.Block
+		quit              chan bool
 	}
 	tests := []struct {
 		name   string
 		fields fields
-		args   args
 	}{
-		// TODO: Add test cases.
+		{
+			"sunny day",
+			fields{
+				"1",
+				make([]node.Block, 2),
+				make([]string, 1),
+				nil,
+				make(chan Message, 100),
+				make(chan node.Block, 10),
+				make(chan bool),
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			m := &Miner{
-				ID:              tt.fields.ID,
-				blocks:          tt.fields.blocks,
-				peers:           tt.fields.peers,
-				rpcHandler:      tt.fields.rpcHandler,
-				messageQueue:    tt.fields.messageQueue,
-				messageChannels: tt.fields.messageChannels,
-				blocksChannels:  tt.fields.blocksChannels,
-				miningBlock:     tt.fields.miningBlock,
-				quit:            tt.fields.quit,
+				ID:                tt.fields.ID,
+				blocks:            tt.fields.blocks,
+				peers:             tt.fields.peers,
+				rpcHandler:        tt.fields.rpcHandler,
+				incomingMsgChan:   tt.fields.incomingMsgChan,
+				incomingBlockChan: tt.fields.incomingBlockChan,
+				quit:              tt.fields.quit,
 			}
-			m.mining(tt.args.block, tt.args.difficulty)
+			m.mining()
+			if m.blocks[1].Header.Date.Equal(time.Time{}) {
+				t.Errorf("date is not defined")
+			}
 		})
 	}
 }
